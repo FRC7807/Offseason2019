6b21aa65849cf4187d1617a092ad1c38134cbe6d
FRC7807 <52088976+FRC7807@users.noreply.github.com>
2019-06-24 16:45:09 -0700

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -23,71 +23,85 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  */
 public class Robot extends TimedRobot {
 
-  WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(0);
-  WPI_TalonSRX leftBackMotor = new WPI_TalonSRX(1);
-  WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(2);
-  WPI_TalonSRX rightBackMotor = new WPI_TalonSRX(3);
-  WPI_TalonSRX liftMotor = new WPI_TalonSRX(4);
-  SpeedControllerGroup left = new SpeedControllerGroup(leftFrontMotor, leftBackMotor);
-  SpeedControllerGroup right = new SpeedControllerGroup(rightFrontMotor, rightBackMotor);
+	WPI_TalonSRX leftFrontMotor;
+	WPI_TalonSRX leftBackMotor;
+	WPI_TalonSRX rightFrontMotor;
+	WPI_TalonSRX rightBackMotor;
+	WPI_TalonSRX liftMotor;
+	
+	private final DifferentialDrive m_robotDrive;
+		
+	SpeedControllerGroup left;
+	SpeedControllerGroup right;
 
-  private final DifferentialDrive m_robotDrive
-      = new DifferentialDrive(left, right);
+	private final Joystick m_stick;
+	Button button1;
+	private final Timer m_timer;
 
-  private final Joystick m_stick = new Joystick(0);
-  Button button1 = new JoystickButton(1);
-  private final Timer m_timer = new Timer();
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		leftFrontMotor = new WPI_TalonSRX(0);
+		leftBackMotor = new WPI_TalonSRX(1);
+		rightFrontMotor = new WPI_TalonSRX(2);
+		rightBackMotor = new WPI_TalonSRX(3);
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-  }
+		liftMotor = new WPI_TalonSRX(4);
 
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-    m_timer.reset();
-    m_timer.start();
-  }
+		left = new SpeedControllerGroup(leftFrontMotor, leftBackMotor);
+		right = new SpeedControllerGroup(rightFrontMotor, rightBackMotor);
+		
+		m_robotDrive = new DifferentialDrive(left, right);
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
-    }
-  }
+		m_stick = new Joystick(0);
+		button1 = new JoystickButton(1);
+		m_timer = new Timer();
+	}
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
-  @Override
-  public void teleopInit() {
-  }
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_timer.reset();
+		m_timer.start();
+	}
 
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
-  @Override
-  public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
-    button1.whenPressed();
-  }
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		// Drive for 2 seconds
+		if (m_timer.get() < 2.0) {
+			m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+		} else {
+			m_robotDrive.stopMotor(); // stop robot
+		}
+	}
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+	/**
+	 * This function is called once each time the robot enters teleoperated mode.
+	 */
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during teleoperated mode.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
 }
